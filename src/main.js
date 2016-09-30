1413b9fac5fec35e077f147cfc61a09116417a13
Francesco Bedussi <francesco@francescobedussi.it>
2016-09-30 18:09:32 +0200

    INIT

--- src/main.js
+++ src/main.js
@@ -279,3 +279,22 @@ storeManager.init().getStore().subscribe(function() {
 
 //Run the app
 ui.init();
+
+
+
+///
+function observeStore(store, select, onChange) {
+  let currentState;
+
+  function handleChange() {
+    let nextState = select(store.getState());
+    if (nextState !== currentState) {
+      currentState = nextState;
+      onChange(currentState);
+    }
+  }
+
+  let unsubscribe = store.subscribe(handleChange);
+  handleChange();
+  return unsubscribe;
+}
